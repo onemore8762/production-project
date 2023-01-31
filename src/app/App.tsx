d7d2f1b497879420ab6a40c6dc8e705748151f47
Denis <denis.churkin.afikei@mail.ru>
2023-02-01 02:30:38 +0300

    add fsd

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,22 @@
 import React, {Suspense} from 'react';
 import {Link, Route, Routes} from 'react-router-dom';
 import './styles/index.scss'
-import {MainPageAsync} from "./page/MainPage/MainPage.async";
-import {AboutPageAsync} from "./page/AboutPage/AboutPage.async";
-import {useTheme} from "./theme/useTheme";
-import {classNames} from "./helpers/classNames/classNames";
-
+import {useTheme} from "app/providers/ThemeProvider";
+import {classNames} from "shared/lib/classNames/classNames";
+import {AboutPage} from 'page/AboutPage';
+import {MainPage} from 'page/MainPage';
 
 const App = () => {
     const {theme, toggleTheme} = useTheme()
-    const bool = true
     return (
         <div className={classNames('app', {}, [theme])}>
-            {/*<Counter/>*/}
             <button onClick={toggleTheme}>TOGGLE</button>
             <Link to={''}>Main Page</Link>
             <Link to={'/about'}>About Page</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={''} element={<MainPageAsync/>}/>
-                    <Route path={'/about'} element={<AboutPageAsync/>}/>
+                    <Route path={''} element={<MainPage/>}/>
+                    <Route path={'/about'} element={<AboutPage/>}/>
                 </Routes>
             </Suspense>
         </div>
