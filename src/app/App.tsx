85bbf58a4117c251ec79aebec51d7cce99b72fda
Denis <denis.churkin.afikei@mail.ru>
2023-02-07 14:58:42 +0300

    add: Sidebar layout and i18n package v 0.14

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,15 +3,18 @@ import './styles/index.scss'
 import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
 import {AppRouter} from "app/providers/router";
-import {Navbar} from "widgets/Navbar/ui/Navbar";
+import {Sidebar} from "widgets/Sidebar";
+import {Navbar} from "widgets/Navbar";
 
 const App = () => {
     const {theme} = useTheme()
     return (
         <div className={classNames('app', {}, [theme])}>
             <Navbar/>
-            <AppRouter/>
-
+            <div className='content-page'>
+                <Sidebar/>
+                <AppRouter/>
+            </div>
         </div>
     );
 };
---
+++ src/widgets/Sidebar/ui/Sidebar.tsx
@@ -0,0 +1,28 @@
+import React, {FC, useState} from 'react';
+import {classNames} from "shared/lib/classNames/classNames";
+import cls from './Sidebar.module.scss'
+import {ThemeSwitcher} from "shared/ui/ThemeSwitcher/ThemeSwitcher";
+
+
+interface SidebarProps {
+    className?: string
+}
+
+
+export const Sidebar: FC<SidebarProps> = ({className}) => {
+    const [collapsed, setCollapsed] = useState(false)
+
+    const toggle = () => {
+        setCollapsed(prevState => !prevState)
+    }
+    return (
+        <div className={classNames(cls.Sidebar, {[cls.collapsed]: collapsed}, [className])}>
+            <button onClick={toggle}> toggle</button>
+            <div className={cls.switchers}>
+                <ThemeSwitcher/>
+                {/*lang switcher*/}
+            </div>
+        </div>
+    );
+};
+
