8395793286a11acceac16d10be9e73abea73f0db
onemore8762 <denis.churkin.afikei@mail.ru>
2023-02-16 23:17:34 +0300

    fix: tests v 0.28

---
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -0,0 +1,22 @@
+import { render } from '@testing-library/react'
+import { type ReactElement } from 'react'
+import { I18nextProvider } from 'react-i18next'
+import i18nForTest from '../../../config/i18n/i18nForTest'
+import { MemoryRouter } from 'react-router-dom'
+
+export interface componentRenderOptions {
+    route?: string
+}
+
+export function componentRender (component: ReactElement, options: componentRenderOptions = {}): any {
+    const {
+        route = '/'
+    } = options
+    return render(
+        <MemoryRouter initialEntries={[route]}>
+            <I18nextProvider i18n={i18nForTest}>
+                {component}
+            </I18nextProvider>
+        </MemoryRouter>
+    )
+}
