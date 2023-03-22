9dd23ce5930e9c6d8aed12cd7e476340d176fbce
onemore8762 <denis.churkin.afikei@mail.ru>
2023-03-22 18:52:01 +0300

    fix: screen test v 0.43

--- src/entities/Profile/ui/ProfileCard/ProfileCard.stories.tsx
+++ src/entities/Profile/ui/ProfileCard/ProfileCard.stories.tsx
@@ -3,7 +3,7 @@ import { type ComponentMeta, type ComponentStory } from '@storybook/react'
 import { ProfileCard } from 'entities/Profile/ui/ProfileCard/ProfileCard'
 import { Country } from 'entities/Country'
 import { Currency } from 'entities/Currency'
-import avatar from 'shared/assets/tests/storybook.jpg'
+
 export default {
     title: 'entities/ProfileCard',
     component: ProfileCard,
@@ -25,7 +25,7 @@ Primary.args = {
         first: 'test',
         city: 'AFS',
         currency: Currency.USD,
-        avatar
+        avatar: 'https://pic.rutubelist.ru/user/3b/27/3b2758ad5492a76b578f7ee072e4e894.jpg'
     }
 }
 export const withError = Template.bind({})
